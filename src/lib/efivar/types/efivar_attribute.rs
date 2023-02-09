97fec9c986faab808b2dcc3834e1d9c63b8c1113
Nicholas Vinson <nvinson234@gmail.com>
2023-02-08 22:32:01 -0500

    Update EfivarAttribute

--- src/lib/efivar/types/efivar_attribute.rs
+++ src/lib/efivar/types/efivar_attribute.rs
@@ -1,5 +1,26 @@
 use std::fmt;
 
+macro_rules! EfivarAttribute_field_value_maps {
+    ($type:ident) => {
+        impl From<EfivarAttribute> for $type {
+            fn from(attr: EfivarAttribute) -> $type {
+                return attr.value as $type;
+            }
+        }
+
+        impl From<&EfivarAttribute> for $type {
+            fn from(attr: &EfivarAttribute) -> $type {
+                return (*attr).value as $type;
+            }
+        }
+    };
+    ($type:ident, $($types:ident),+) => {
+        EfivarAttribute_field_value_maps!($type);
+        EfivarAttribute_field_value_maps!($($types),+);
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct EfivarAttribute {
     name: &'static str,
     value: u64,
@@ -11,8 +32,26 @@ impl fmt::Display for EfivarAttribute {
     }
 }
 
+EfivarAttribute_field_value_maps!(u128, u64, u32, u16, i128, i64, i32);
+
+impl From<EfivarAttribute> for String {
+    fn from(attr: EfivarAttribute) -> String {
+        return String::from(attr.name);
+    }
+}
+
+impl From<&EfivarAttribute> for String {
+    fn from(attr: &EfivarAttribute) -> String {
+        return String::from((*attr).name);
+    }
+}
+
 impl EfivarAttribute {
     pub(crate) const fn init(name: &'static str, value: u64) -> Self {
         return Self {name, value};
+    }
+
+    pub fn name(&self) -> &'static str {
+        return self.name;
     }
 }
