4910c3cfda015d2ff5d175124fbe8ab8ca2233a7
Jturnerusa <50645473+Jturnerusa@users.noreply.github.com>
2023-04-26 23:00:39 -0400

    use the enum for the error directly instead of the struct

--- src/lib/efivar/types/efi_guid_error.rs
+++ src/lib/efivar/types/efi_guid_error.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::fmt;
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
-pub enum EfiGuidErrorKind {
+pub enum EfiGuidError {
     BadFormat,
     SliceLengthTooLong,
     SliceLengthTooShort,
@@ -10,43 +10,27 @@ pub enum EfiGuidErrorKind {
     VecLengthTooShort,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Copy)]
-pub struct EfiGuidError(EfiGuidErrorKind);
-
-impl Error for EfiGuidError {}
-
 impl fmt::Display for EfiGuidError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0 {
-            EfiGuidErrorKind::BadFormat => write!(
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::BadFormat => write!(
                 f,
                 "bad format. Correct format is xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
             ),
-            EfiGuidErrorKind::SliceLengthTooLong => {
+            Self::SliceLengthTooLong => {
                 write!(f, "source slice too long. Slice must have a size of 16")
             }
-            EfiGuidErrorKind::SliceLengthTooShort => {
+            Self::SliceLengthTooShort => {
                 write!(f, "source slice too short. Slice must have a size of 16")
             }
-            EfiGuidErrorKind::VecLengthTooLong => {
+            Self::VecLengthTooLong => {
                 write!(f, "source vector too long. Vector must have a size of 16")
             }
-            EfiGuidErrorKind::VecLengthTooShort => {
+            Self::VecLengthTooShort => {
                 write!(f, "source vector too short. Vector must have a size of 16")
             }
         }
     }
 }
 
-pub const BAD_FORMAT: EfiGuidError = EfiGuidError(EfiGuidErrorKind::BadFormat);
-
-pub const SRC_SLICE_LENGTH_TOO_LONG: EfiGuidError =
-    EfiGuidError(EfiGuidErrorKind::SliceLengthTooLong);
-
-pub const SRC_SLICE_LENGTH_TOO_SHORT: EfiGuidError =
-    EfiGuidError(EfiGuidErrorKind::SliceLengthTooShort);
-
-pub const SRC_VEC_LENGTH_TOO_LONG: EfiGuidError = EfiGuidError(EfiGuidErrorKind::VecLengthTooLong);
-
-pub const SRC_VEC_LENGTH_TOO_SHORT: EfiGuidError =
-    EfiGuidError(EfiGuidErrorKind::VecLengthTooShort);
+impl Error for EfiGuidError {};
