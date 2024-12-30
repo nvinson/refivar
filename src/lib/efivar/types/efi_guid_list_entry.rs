9d581011f07a25e7cbabf7b2f271b6a0b1003863
Nicholas Vinson <nvinson234@gmail.com>
2024-12-30 08:24:14 -0500

    Finish EfiGuidListEntry handling
    Signed-off-by: Nicholas Vinson <nvinson234@gmail.com>

--- src/lib/efivar/types/efi_guid_list_entry.rs
+++ src/lib/efivar/types/efi_guid_list_entry.rs
@@ -1,6 +1,8 @@
 use crate::types::efi_guid::EfiGuid;
-use serde::de::{Deserialize};
+use crate::types::EfiGuidError;
+use serde::de::{Deserialize, Error, MapAccess, Visitor};
 use std::fmt;
+use std::str::FromStr;
 
 #[derive(PartialEq)]
 pub struct EfiGuidListEntry {
@@ -9,12 +11,69 @@ pub struct EfiGuidListEntry {
     pub description: String,
 }
 
+struct EfiGuidListEntryVisitor {}
+
+impl EfiGuidListEntryVisitor {
+    fn new() -> Self {
+        EfiGuidListEntryVisitor{}
+    }
+}
+
+impl<'de> Visitor<'de> for EfiGuidListEntryVisitor {
+    type Value = EfiGuidListEntry;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("EfiGuiListEntry object")
+    }
+
+    fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        let mut guid: Option<Result<EfiGuid, EfiGuidError>> = None;
+        let mut name: Option<String> = None;
+        let mut description: Option<String> = None;
+
+        while let Some((key, value)) = access.next_entry::<String, String>()? {
+            if key == "description" {
+                description = Some(value);
+            } else if key == "guid" {
+                guid = Some(EfiGuid::from_str(&value));
+            } else if key ==  "name" {
+                name = Some(value);
+            } else {
+                eprintln!("Unknown key: {} with value {}", key, value)
+            }
+        }
+
+        if description.is_none() {
+            return Err(M::Error::custom(&"description missing"));
+        };
+        if guid.is_none() {
+            return Err(M::Error::custom(&"guid missing"));
+        };
+        if name.is_none() {
+            return Err(M::Error::custom(&"name missing"));
+        };
+
+        let efiGuidResult = guid.unwrap();
+        if efiGuidResult.is_err() {
+            return Err(M::Error::custom(efiGuidResult.unwrap_err()));
+        }
+        Ok(EfiGuidListEntry{
+            description: description.unwrap(),
+            guid: efiGuidResult.unwrap(),
+            name: name.unwrap(),
+        })
+    }
+}
+
 impl<'de> Deserialize<'de> for EfiGuidListEntry {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
-        todo!()
+        deserializer.deserialize_map(EfiGuidListEntryVisitor::new())
     }
 }
 
